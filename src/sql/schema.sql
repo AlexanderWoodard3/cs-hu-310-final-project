48716db35f8975817bf9542bfe56d8aa925bee96
AlexanderWoodard3 <77296754+AlexanderWoodard3@users.noreply.github.com>
2021-03-20 20:48:42 -0600

    Update schema.sql

--- src/sql/schema.sql
+++ src/sql/schema.sql
@@ -8,6 +8,8 @@ DROP TABLE IF EXISTS academic_titles;
 DROP TABLE IF EXISTS students;
 DROP TABLE IF EXISTS classes
 DROP FUNCTION IF EXISTS convert_to_grade_point;
+
+
 CREATE TABLE IF NOT EXISTS classes(
  class_id INT AUTO_INCREMENT,
  name VARCHAR(50) NOT NULL,
@@ -16,10 +18,65 @@ CREATE TABLE IF NOT EXISTS classes(
  maximum_students INT DEFAULT 10,
  PRIMARY KEY(class_id)
 );
+
+
 CREATE TABLE IF NOT EXISTS students(
  student_id INT AUTO_INCREMENT,
  first_name VARCHAR(30) NOT NULL,
  last_name VARCHAR(50) NOT NULL,
  birthdate DATE,
  PRIMARY KEY (student_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS academic_titles(
+ academic_title_id INT AUTO_INCREMENT NOT NULL,
+ title VARCHAR(255) NOT NULL,
+ PRIMARY KEY (academic_title_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS instructors(
+ instructor_id INT AUTO_INCREMENT NOT NULL,
+ first_name VARCHAR(80) NOT NULL,
+ last_name VARCHAR(80) NOT NULL,
+ birthdate DATE,
+ PRIMARY KEY (instructor_id)
+ FOREIGN KEY (academic_title_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS terms(
+ term_id INT AUTO_INCREMENT NOT NULL,
+ name VARCHAR(80) NOT NULL,
+ PRIMARY KEY (term_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS class_sections(
+ class_section_id INT AUTO_INCREMENT NOT NULL,
+ class_is INT NOT NULL,
+ instructor_id INT NOT NULL,
+ term_id NOT NULL,
+ PRIMARY KEY (class_section_id)
+ FOREIGN KEY (class_id, instructor_id, term_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS grades(
+ grade_id INT AUTO_INCREMENT NOT NULL,
+ letter_grade VARCHAR(2) NOT NULL,
+ PRIMARY KEY (grade_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS class_registrations(
+ class_registration_id INT AUTO_INCREMENT NOT NULL,
+ class_section_id INT NOT NULL,
+ student_id INT NOT NULL,
+ student_id INT NOT NULL,
+ grade_id INT,
+ signup_timestamp datetime DEFAULT CURRENT_TIMESTAMP;
+ PRIMARY KEY (student_id)
+ SPECIAL KEY (
 );
