cc8fee676fd9577233e63807a1fc6997b3f0d2a2
AlexanderWoodard3 <77296754+AlexanderWoodard3@users.noreply.github.com>
2021-03-21 19:46:31 -0600

    Update reporting_queries.sql

--- src/sql/reporting_queries.sql
+++ src/sql/reporting_queries.sql
@@ -1,8 +1,59 @@
 /* Put your final project reporting queries here */
 USE cs_hu_310_final_project;
 
--- Example (remove before submitting)
--- Get all students
+ -- Calculate the GPA for a student given their student_id (use student_id = 1)
+ 
 SELECT
-    *
-FROM students;
+	student_id = 1
+    (SUM(gp.points * c.credits) / s.total_credits) AS GPA
+FROM
+	students
+	JOIN class_registrations t ON s.student_id = t.student_id
+	JOIN grades c ON t.course_id = c.course_id
+	JOIN grade_point gp ON gp.grade = t.grade
+GROUP BY s.student_id, s.total_credits;
+
+ -- Calculate the GPA for each student (across all classes and all terms)
+ 
+SELECT
+	students.student_id
+    (SUM(gp.points * c.credits) / s.total_credits) AS GPA
+FROM
+	students
+	JOIN class_registrations t ON s.student_id = t.student_id
+	JOIN grades c ON t.course_id = c.course_id
+	JOIN grade_point gp ON gp.grade = t.grade
+GROUP BY s.student_id, s.total_credits;
+ 
+  -- Calculate the avg GPA for each class
+  
+  
+  
+  -- Calculate the avg GPA for each class and term
+   
+   
+   
+  -- List all the classes being taught by an instructor (use instructor_id = 1)
+    
+    SELECT
+		instructor_id = 1
+	FROM
+    
+   -- List all classes with terms and instructors
+    
+    SELECT 
+		* 
+	FROM
+		classes
+        join classes c ON terms
+        join classes c ON instructors
+        
+    
+    -- Calculate the remaining space left in a class
+
+	SELECT
+		classes.maximum_students
+        SUM(10 - classes.maximum_students) AS space_remaining
+	FROM
+		classes
+        join classes c ON space_remaining
